1b07c90d0cc9cab5e9340f00fe5663f25658e1b3
Mess <45535471+Messier1071@users.noreply.github.com>
2025-06-27 13:45:23 -0300

    ignore

--- ServiceElevator.X/nbproject/Makefile-variables.mk
+++ ServiceElevator.X/nbproject/Makefile-variables.mk
@@ -6,8 +6,8 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=Cexec2.X.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/Cexec2.X.production.hex
+CND_ARTIFACT_NAME_default=serviceElevator.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/serviceElevator.X.production.hex
 CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
-CND_PACKAGE_NAME_default=cexec2.x.tar
-CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/cexec2.x.tar
+CND_PACKAGE_NAME_default=serviceelevator.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/serviceelevator.x.tar
--- ServiceElevator.X/nbproject/Package-default.bash
+++ ServiceElevator.X/nbproject/Package-default.bash
@@ -10,9 +10,9 @@ CND_CONF=default
 CND_DISTDIR=dist
 TMPDIR=build/${CND_CONF}/${IMAGE_TYPE}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/Cexec2.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-OUTPUT_BASENAME=Cexec2.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-PACKAGE_TOP_DIR=cexec2.x/
+OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/serviceElevator.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+OUTPUT_BASENAME=serviceElevator.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+PACKAGE_TOP_DIR=serviceelevator.x/
 
 # Functions
 function checkReturnCode
@@ -57,15 +57,15 @@ mkdir -p ${TMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory ${TMPDIR}/cexec2.x/bin
+makeDirectory ${TMPDIR}/serviceelevator.x/bin
 copyFileToTmpDir "${OUTPUT_PATH}" "${TMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/package/cexec2.x.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/package/serviceelevator.x.tar
 cd ${TMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/cexec2.x.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/serviceelevator.x.tar *
 checkReturnCode
 
 # Cleanup
